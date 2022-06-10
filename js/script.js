06a386cf01a6fda5526b15b96e9d82f44749f7b5
Albina Abykanova <abykanovaalbina@gmail.com>
2022-06-10 20:39:21 +0300

    portfolio,

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*  =============  typing animation   =============*/
+var typed = new Typed(".typing",{
+    strings:["Web Desinger", "Web Devoloper", "FrontendDevoloper", "ReactDevoloper","JSDevoloper", "TailwindCssDevoloper"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
